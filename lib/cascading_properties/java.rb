4e5fced91402791b23627e2a0f7ff4ef5148a92a
akm <akima@groovenauts.jp>
2012-04-04 17:52:42 +0900

    modify file name

--- lib/cascading_properties/java.rb
+++ lib/cascading_properties/java.rb
@@ -3,6 +3,7 @@ require 'cascading_properties'
 
 java.util.Properties.instance_eval do
   def load_dsl(dsl_text)
-    CascadingProperties.load(dsl_text)
+    node = CascadingProperties.load(dsl_text)
+    node.to_properties
   end
 end
